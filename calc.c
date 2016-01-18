c53d37b6969220a7ad16463274810d427fd6006d
Muhammad Adam <P06235136@GWHXX583LIN011W.lec-admin.dmu.ac.uk>
2016-01-18 13:23:20 +0000

    all functions added

--- calc.c
+++ calc.c
@@ -2,13 +2,30 @@
 
 int main () {
 
-int x, y, sum;
+int x, y;
+char op;
 
-printf("enter two numbers to add\n");
+printf("enter two numbers\n");
 scanf("%i %i", &x, &y);
 
-sum = x + y;
-printf("%i + %i = %i\n", x, y, sum);
+printf("enter + or - or * or /\n");
+
+   switch(op) {
+      case 'a' :
+         printf("%i + %i = %i!\n", x, y, x + y);
+         break;
+      case 's' :
+         printf("%i - %i = %i\n", x, y, x - y );
+         break;
+      case 'm' :
+         printf("%i * %i = %i\n", x, y, x * y );
+         break;
+      case 'd' :
+         printf("%i / %i = %i\n", x, y, x / y );
+         break;
+      default :
+         printf("Invalid request\n" );
+   }
 
 return 0;
 
