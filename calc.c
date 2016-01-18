13226032f84a3baead99059ea2ae11f469bf3d39
Muhammad Adam <P06235136@GWHXX583LIN011W.lec-admin.dmu.ac.uk>
2016-01-18 13:14:07 +0000

    addition

---
+++ calc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+
+int x, y, sum;
+
+printf("enter two numbers to add\n");
+scanf("%i %i", &x, &y);
+
+sum = x + y;
+printf("%i + %i = %i\n", x, y, sum);
+
+return 0;
+
+}
