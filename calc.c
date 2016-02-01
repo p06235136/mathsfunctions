02993dab1310e1b6258c8d4d6acdf1e177b501d5
Muhammad Adam <P06235136@GWHXX583LIN011W.lec-admin.dmu.ac.uk>
2016-02-01 13:11:42 +0000

    conflict

--- calc.c
+++ calc.c
@@ -1,31 +1,9 @@
+//This is the line causing issue in the conflict branch
+
 #include <stdio.h>
 
 int main () {
 
-int x, y;
-char op;
-
-printf("enter two numbers\n");
-scanf("%i %i", &x, &y);
-
-printf("enter + or - or * or /\n");
-
-   switch(op) {
-      case 'a' :
-         printf("%i + %i = %i!\n", x, y, x + y);
-         break;
-      case 's' :
-         printf("%i - %i = %i\n", x, y, x - y );
-         break;
-      case 'm' :
-         printf("%i * %i = %i\n", x, y, x * y );
-         break;
-      case 'd' :
-         printf("%i / %i = %i\n", x, y, x / y );
-         break;
-      default :
-         printf("Invalid request\n" );
-   }
 
 return 0;
 
